671a8cf434c4a7adbec75fd4e49965b2dbc8e61c
dyson <dysonthomas112@gmail.com>
2020-08-13 19:40:36 +0530

    13082020

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,14 @@
 const functions = require('firebase-functions');
 const { Telegraf } = require('telegraf')
 const axios = require('axios');
+require("dotenv").config(); 
 
-const bot = new Telegraf('')
+const bot = new Telegraf(process.env.TG_KEY)
 bot.start((ctx) => ctx.reply('Welcome Search With Your Place Name'))
 bot.hears('hi', (ctx) => ctx.reply('Hey From Dyson'))
 bot.on('text', (ctx) =>{
     const params = {
-        access_key:"",
+        access_key:process.env.API_KEY,
         query: ctx.update.message.text
       } 
       axios.get('http://api.weatherstack.com/current',{params})
